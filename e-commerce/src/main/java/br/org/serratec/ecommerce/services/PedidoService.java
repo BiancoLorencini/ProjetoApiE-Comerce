0cc6efd75d43464ed6212b5679fcdb1eaf870f4c
Heitor Bittencourt <heitorsbittencourt@gmail.com>
2024-05-24 18:30:08 -0300

    feat: Criação método calcularValorTotal

--- e-commerce/src/main/java/br/org/serratec/ecommerce/services/PedidoService.java
+++ e-commerce/src/main/java/br/org/serratec/ecommerce/services/PedidoService.java
@@ -1,5 +1,6 @@
 package br.org.serratec.ecommerce.services;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,23 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.org.serratec.ecommerce.dtos.PedidoDTO;
+import br.org.serratec.ecommerce.entities.ItemPedido;
 import br.org.serratec.ecommerce.entities.Pedido;
+import br.org.serratec.ecommerce.repository.ItemPedidoRepository;
 import br.org.serratec.ecommerce.repository.PedidoRepository;
 
 @Service
 public class PedidoService {
 	@Autowired
 	PedidoRepository pedidoRepository;
-	
+
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
+
 	public List<Pedido> findAll() {
 		return pedidoRepository.findAll();
 	}
-	
+
 	public List<PedidoDTO> findAllDto() {
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		List<PedidoDTO> pedidosDto = new ArrayList<>();
-		
-		for(Pedido pedido: pedidos) {
+
+		for (Pedido pedido : pedidos) {
 			PedidoDTO pedidoDto = new PedidoDTO();
 			pedidoDto.setDataPedido(pedido.getDataPedido());
 			pedidoDto.setDataEnvio(pedido.getDataEnvio());
@@ -31,8 +37,8 @@ public class PedidoService {
 			pedidoDto.setStatus(pedido.getStatus());
 			pedidoDto.setValorTotal(pedido.getValorTotal());
 			pedidoDto.setCliente(pedido.getCliente());
-			
-			pedidosDto.add(pedidoDto);			
+
+			pedidosDto.add(pedidoDto);
 		}
 		return pedidosDto;
 	}
@@ -40,15 +46,16 @@ public class PedidoService {
 	public Pedido findById(Integer id) {
 		return pedidoRepository.findById(id).orElse(null);
 	}
-	
+
 	public Pedido save(Pedido pedido) {
+		calcularValorTotal(pedido);
 		return pedidoRepository.save(pedido);
 	}
 
 	public Pedido update(Pedido pedido) {
 		return pedidoRepository.save(pedido);
 	}
-	
+
 	public boolean deleteById(Integer id) {
 		if (pedidoRepository.existsById(id)) {
 			pedidoRepository.deleteById(id);
@@ -66,5 +73,16 @@ public class PedidoService {
 	public long count() {
 		return pedidoRepository.count();
 	}
-}
 
+	public void calcularValorTotal(Pedido pedido) {
+		List<ItemPedido> itensPedidos = itemPedidoRepository.findAll();
+
+		BigDecimal valorTotal = BigDecimal.ZERO;
+		for (ItemPedido item : itensPedidos) {
+			valorTotal = valorTotal.add(item.getValorLiquido());
+		}
+		
+		pedido.setValorTotal(valorTotal);
+
+	}
+}
