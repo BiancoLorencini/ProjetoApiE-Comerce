429d5d817e8a6aad8638030640711402a73119e7
Bianco <arthnilo@gmail.com>
2024-05-24 21:39:19 -0300

    feat: correções

--- e-commerce/src/main/java/br/org/serratec/ecommerce/dtos/EnderecoDTO.java
+++ e-commerce/src/main/java/br/org/serratec/ecommerce/dtos/EnderecoDTO.java
@@ -1,71 +1,68 @@
 package br.org.serratec.ecommerce.dtos;
 
 public class EnderecoDTO {
-	   private String cep;
-	    private String logradouro;
-	    private String bairro;
-	    private String localidade;  // Nome da cidade
-		private String uf;
-		private String numero;
-		private String complemento;
-		
-		
-	    
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String localidade; // Nome da cidade
+	private String uf;
+	private String numero;
+	private String complemento;
 
-	    public String getCep() {
-	        return cep;
-	    }
+	public String getCep() {
+		return cep;
+	}
 
-	    public void setCep(String cep) {
-	        this.cep = cep;
-	    }
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 
-	    public String getLogradouro() {
-	        return logradouro;
-	    }
+	public String getLogradouro() {
+		return logradouro;
+	}
 
-	    public void setLogradouro(String logradouro) {
-	        this.logradouro = logradouro;
-	    }
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
 
-	    public String getBairro() {
-	        return bairro;
-	    }
+	public String getBairro() {
+		return bairro;
+	}
 
-	    public void setBairro(String bairro) {
-	        this.bairro = bairro;
-	    }
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
 
-	    public String getLocalidade() {
-	        return localidade;
-	    }
+	public String getLocalidade() {
+		return localidade;
+	}
 
-	    public void setLocalidade(String localidade) {
-	        this.localidade = localidade;
-	    }
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
 
-	    public String getUf() {
-	        return uf;
-	    }
+	public String getUf() {
+		return uf;
+	}
 
-	    public void setUf(String uf) {
-	        this.uf = uf;
-	    }
-	    public String getNumero() {
-	    	return numero;
-	    }
-	    
-	    public void setNumero(String numero) {
-	    	this.numero = numero;
-	    }
-	    public String getComplemento() {
-	    	return complemento;
-	    }
-	    
-	    public void setComplemento(String complemento) {
-	    	this.complemento = complemento;
-	    }
-	    
-	
-    
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
 }
